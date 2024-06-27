f71875af734d79a03ce64dcf6305a707d18046f3
Ivy Nyambura <ikagam018@gmail.com>
2024-06-27 22:17:42 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function (a,b){
+    return a/b;
+}
+console.log(divide("2000 / 100"));
+
+const square = (num1) => num1*num1;
+console.log(square(2));
+
+const add = (num2, num3) => num2 + num3;
+console.log(add(5,5));
